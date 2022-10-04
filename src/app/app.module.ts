5137dd11510c56513302ac403bd5b6c03e503887
victor-val <vval26v@gmail.com>
2022-10-04 16:46:37 +0200

    add book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,9 @@ import { BooksListComponent } from './components/books-list/books-list.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookService } from './../../services/book.service';
+import { FormGroup, FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  bookForm: FormGroup;
+  
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private crudService: BookService
+  ) { 
+    this.bookForm = this.formBuilder.group({
+      name: [''],
+      price: [''],
+      description: ['']
+    })
   }
 
+  ngOnInit() { }
+  
+  onSubmit(): any {
+    this.crudService.addBook(this.bookForm.value)
+    .subscribe({
+      complete: () => {
+        console.log('Data added successfully!')
+        this.ngZone.run(() => this.router.navigateByUrl('/book-list'))
+      }, error: (err) => {
+        console.log(err);
+      }
+   });
+  }
 }
