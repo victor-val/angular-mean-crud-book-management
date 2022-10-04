4eeb2836083e0e702e19b946ed75ca6ee7b8d5fa
victor-val <vval26v@gmail.com>
2022-10-04 16:03:15 +0200

    main screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { BooksListComponent } from './components/books-list/books-list.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
