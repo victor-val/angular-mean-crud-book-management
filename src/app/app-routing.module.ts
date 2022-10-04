21b5e7119bba543aa8aca4afd07e27fc7d250a3c
victor-val <vval26v@gmail.com>
2022-10-04 11:15:52 +0200

    Create CRUD routes and routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'books-list' },
+  { path: 'books-list', component: BooksListComponent },
+  { path: 'add-book', component: AddBookComponent },
+  { path: 'edit-book/:id', component: BookDetailComponent }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
@@ -14,7 +14,8 @@ import { BooksListComponent } from './components/books-list/books-list.component
     BooksListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
